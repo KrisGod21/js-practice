43d84cbd66486f29410f5e69b4804418204344e0
Krishna <krishnar.sharmanew@gmail.com>
2026-08-10 22:19:59 +0530

    testing-backend

--- backend time/server.js
+++ backend time/server.js
@@ -1,11 +1,17 @@
-const express= require('express');
-const app = express();
 
-app.get("/",(req,res) =>{
-    res.send("Hello world");
-});
+const app = require("./src/app")
 
-app.get("/about",(req,res)=>{
-    res.send("about page")
+// app.get("/",(req,res) =>{
+//     res.send("Hello world");
+// });
+
+// app.get("/about",(req,res)=>{
+//     res.send("about page")
+// });
+const notes =[]
+app.post('/notes',(req,res)=>{
+    console.log(req.body)
+})
+app.listen(3000,() =>{
+    console.log("Server is running");
 });
-app.listen(3000);
--- backend time/src/app.js
+++ backend time/src/app.js
@@ -1,3 +1,5 @@
 //server ko create karna
 const express = require('express');
 const app = express()
+
+module.exports = app;
