5fa13a262c884d1b328103c5cb2b7a0b7a2895bb
Krishna <krishnar.sharmanew@gmail.com>
2026-08-11 22:23:43 +0530

    basic-mongodb-doneee

--- backend time/src/app.js
+++ backend time/src/app.js
@@ -1,5 +1,43 @@
 //server ko create karna
 const express = require('express');
 const app = express()
-
+const noteModel = require("./db/models/note.model")
 module.exports = app;
+app.use(express.json());
+app.post("/notes", async (req,res)=>{
+    const data = req.body
+    await noteModel.create({
+        title:data.title,
+        description:data.description 
+    })
+    res.status(201).json({
+        message:"Node created successfully"
+    })
+})
+
+app.get("/notes",async (req,res)=>{
+    const notes = await noteModel.find()//gives an array
+    res.status(200).json({
+        message : "nodes fetched successfully",
+        notes : notes
+    })
+})
+
+app.delete("/notes/:id",async (req,res)=>{
+    const id = req.params.id
+    await noteModel.findOneAndDelete({
+        _id : id
+    })
+    res.status(200).json({
+        message:"Node deleted"
+    })
+})
+
+app.patch("/notes/:id",async(req,res)=>{
+    const id = req.params.id
+    const description = req.body.description
+    await noteModel.findOneAndUpdate({ _id : id}, {description : description})
+    res.status(200).json({
+        message:"Node updated"
+    })
+})
--- backend time/src/db/models/note.model.js
+++ backend time/src/db/models/note.model.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
 
+
 const noteSchema = new mongoose.Schema({
     title:String,
     description: String,
     age:Number,
     dob:Date
 })
+
+const noteModel = mongoose.model("note",noteSchema)
+module.exports = noteModel;
