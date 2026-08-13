c05db33bfd0df18c2bc4c5255ab2dce73f71f8ac
Krishna <krishnar.sharmanew@gmail.com>
2026-08-13 22:48:57 +0530

    Stop tracking db.js

--- cloud-backend/src/db/db.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose");
-const connectDB = 
-async function connectDB(){
-    await mongoose.connect('mongodb+srv://krishnarsharmanew_db_user:ELs6MN7Cm_Zt22F@my-first-cluster.1cnejrv.mongodb.net/project-1')
-}
--- cloud-backend/src/models/post.model.js
+++ cloud-backend/src/models/post.model.js
@@ -1,1 +1,8 @@
 const mongoose = require("mongoose")
+
+const postSchema = new mongoose.Scheme({
+    image:String,
+    caption:String
+})
+
+const postModel = mongoose.model("post",postSchema);
