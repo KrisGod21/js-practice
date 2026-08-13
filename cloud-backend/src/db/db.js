64ac3a416158d5d24072cedba4cb9965bb46a7e4
Krishna Sharma <krishnar.sharmanew@gmail.com>
2026-08-13 21:41:34 +0530

    Update MongoDB connection string in db.js

--- cloud-backend/src/db/db.js
+++ cloud-backend/src/db/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 const connectDB = 
 async function connectDB(){
-    await mongoose.connect('mongodb+srv://krishnarsharmanew_db_user:ELs6MN7Cm_Zt22F@my-first-cluster.1cnejrv.mongodb.net/project-1')
+    await mongoose.connect('*')
 }
