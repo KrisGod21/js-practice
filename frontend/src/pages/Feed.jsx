29f8fbbc661eca6047c04f3ab587c55b3be4c8bd
Krishna <krishnar.sharmanew@gmail.com>
2026-08-14 23:13:53 +0530

    idk

---
+++ frontend/src/pages/Feed.jsx
@@ -0,0 +1,17 @@
+import React,{useState} from 'react'
+
+const Feed = () => {
+    const [post, setpost] = useState([
+        {
+            _id:"1",
+            image: 
+        }
+    ])
+  return (
+    <section className='feed-section'>
+        <h1>Feed</h1>
+    </section>
+  )
+}
+
+export default Feed
